dd1179384f4324ed280d00a80b95bf508f9ac219
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-22 13:00:01 +0900

    :wrench: gradle.kts を整理

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import io.kotless.plugin.gradle.dsl.kotless
+
 plugins {
-  kotlin("jvm") version "1.8.21"
-  application
+  kotlin("jvm") version "1.8.21" apply true
+  id("io.kotless") version "0.2.0" apply true
 }
 
 group = "com.doggy"
@@ -8,9 +10,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
   mavenCentral()
+  maven(url = uri("https://packages.jetbrains.team/maven/p/ktls/maven"))
 }
 
 dependencies {
+  implementation("io.kotless", "kotless-lang", "0.2.0")
+  implementation("io.kotless", "kotless-lang-aws", "0.2.0")
+
   testImplementation(kotlin("test"))
 }
 
@@ -18,10 +24,34 @@ tasks.test {
   useJUnitPlatform()
 }
 
-kotlin {
-  jvmToolchain(11)
+kotless {
+  config {
+    aws {
+      storage {
+        bucket = "kotless.s3.example.com"
+      }
+      profile = "example"
+      region = "eu-west-1"
+    }
+  }
+
+  webapp {
+    lambda {
+      kotless {
+        packages = setOf("com.example.kotless")
+      }
+      memoryMb = 1024
+      timeoutSec = 120
+    }
+  }
+
+  extensions {
+    terraform {
+      allowDestroy = true
+    }
+  }
 }
 
-application {
-  mainClass.set("MainKt")
+kotlin {
+  jvmToolchain(11)
 }
